a2464029519a656ea2de2037e850316a48ef571e
walison matheus <walisom360@hotmail.com>
2022-12-29 22:34:17 -0300

    add cypress and storybook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
-import { CardProduct } from "../components/CardProduct";
-import { Header } from "../components/Header";
-
-import { Container, Wrapper, GridList } from "../styles/pages/home";
-
-interface Product {
-  image_url: string;
-}
-
-interface ProductData {
-  allProducts: Product[];
-}
-
-const GET_PRODUCTS = gql`
-  query {
-    allProducts(page: 1, perPage: 10) {
-      id
-      name
-      description
-      image_url
-      price_in_cents
-      sales
-      created_at
-    }
-  }
-`;
+// pages/index.js
+import Link from "next/link";
 
 export default function Home() {
-  const { loading, error, data } = useQuery<ProductData>(GET_PRODUCTS);
-
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
-
   return (
-    <Container>
-      <Header />
-
-      <Wrapper>
-        <GridList>
-          {data?.allProducts?.map((product, index) => (
-            <CardProduct product={product} key={index} />
-          ))}
-        </GridList>
-      </Wrapper>
-    </Container>
+    <nav>
+      <Link href="/about">About</Link>
+    </nav>
   );
 }
